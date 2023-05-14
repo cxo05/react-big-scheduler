d184f4f2ddd2332d6ecf456aaa120c0d7c4cdcba
Xavier <xavieroh05@gmail.com>
2023-05-14 16:07:21 +0800

    Migrate to latest versions of webpack etc

--- src/DnDContext.js
+++ src/DnDContext.js
@@ -2,135 +2,135 @@ import { DropTarget } from 'react-dnd'
 import { getPos } from './Util'
 import { DnDTypes } from './DnDTypes'
 import { CellUnit, DATETIME_FORMAT } from './index'
-import { ViewType } from './ViewType'
+import ViewType from './ViewType'
 
 export default class DnDContext {
-    constructor(sources, DecoratedComponent) {
-        this.sourceMap = new Map();
-        sources.forEach((item) => {
-            this.sourceMap.set(item.dndType, item);
-        })
-        this.DecoratedComponent = DecoratedComponent;
-    }
+  constructor(sources, DecoratedComponent) {
+    this.sourceMap = new Map();
+    sources.forEach((item) => {
+      this.sourceMap.set(item.dndType, item);
+    })
+    this.DecoratedComponent = DecoratedComponent;
+  }
 
-    getDropSpec = () => {
-        return {
-            drop: (props, monitor, component) => {
-                const { schedulerData, resourceEvents } = props;
-                const { cellUnit, localeDayjs } = schedulerData;
-                const type = monitor.getItemType();
-                const pos = getPos(component.eventContainer);
-                let cellWidth = schedulerData.getContentCellWidth();
-                let initialStartTime = null, initialEndTime = null;
-                if (type === DnDTypes.EVENT) {
-                    const initialPoint = monitor.getInitialClientOffset();
-                    let initialLeftIndex = Math.floor((initialPoint.x - pos.x) / cellWidth);
-                    initialStartTime = resourceEvents.headerItems[initialLeftIndex].start;
-                    initialEndTime = resourceEvents.headerItems[initialLeftIndex].end;
-                    if (cellUnit !== CellUnit.Hour)
-                        initialEndTime = localeDayjs(new Date(resourceEvents.headerItems[initialLeftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
-                }
-                const point = monitor.getClientOffset();
-                let leftIndex = Math.floor((point.x - pos.x) / cellWidth);
-                let startTime = resourceEvents.headerItems[leftIndex].start;
-                let endTime = resourceEvents.headerItems[leftIndex].end;
-                if (cellUnit !== CellUnit.Hour)
-                    endTime = localeDayjs(new Date(resourceEvents.headerItems[leftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
-
-                return {
-                    slotId: resourceEvents.slotId,
-                    slotName: resourceEvents.slotName,
-                    start: startTime,
-                    end: endTime,
-                    initialStart: initialStartTime,
-                    initialEnd: initialEndTime
-                };
-            },
-
-            hover: (props, monitor, component) => {
-                
-                const { schedulerData, resourceEvents, movingEvent } = props;
-                const { cellUnit, config, viewType, localeDayjs } = schedulerData;
-                this.config = config;
-                const item = monitor.getItem();
-                const type = monitor.getItemType();
-                const pos = getPos(component.eventContainer);
-                let cellWidth = schedulerData.getContentCellWidth();
-                let initialStart = null, initialEnd = null;            
-                if (type === DnDTypes.EVENT) {
-                    const initialPoint = monitor.getInitialClientOffset();
-                    let initialLeftIndex = Math.floor((initialPoint.x - pos.x) / cellWidth);
-                    initialStart = resourceEvents.headerItems[initialLeftIndex].start;
-                    initialEnd = resourceEvents.headerItems[initialLeftIndex].end;
-                    if (cellUnit !== CellUnit.Hour)
-                        initialEnd = localeDayjs(new Date(resourceEvents.headerItems[initialLeftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
-                }
-                const point = monitor.getClientOffset();
-                let leftIndex = Math.floor((point.x - pos.x) / cellWidth);
-                if (!resourceEvents.headerItems[leftIndex]) {
-                    return;
-                }
-                let newStart = resourceEvents.headerItems[leftIndex].start;
-                let newEnd = resourceEvents.headerItems[leftIndex].end;
-                if (cellUnit !== CellUnit.Hour)
-                    newEnd = localeDayjs(new Date(resourceEvents.headerItems[leftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
-                let slotId = resourceEvents.slotId, slotName = resourceEvents.slotName;
-                let action = 'New';
-                let isEvent = type === DnDTypes.EVENT;
-                if (isEvent) {
-                    const event = item;
-                    if (config.relativeMove) {
-                        newStart = localeDayjs(event.start).add(localeDayjs(newStart).diff(localeDayjs(new Date(initialStart))), 'ms').format(DATETIME_FORMAT);
-                    } else {
-                        if (viewType !== ViewType.Day) {
-                            let tmpDayjs = localeDayjs(newStart);
-                            newStart = localeDayjs(event.start).year(tmpDayjs.year()).month(tmpDayjs.month()).date(tmpDayjs.date()).format(DATETIME_FORMAT);
-                        }
-                    }
-                    newEnd = localeDayjs(newStart).add(localeDayjs(event.end).diff(localeDayjs(event.start)), 'ms').format(DATETIME_FORMAT);
-
-                    //if crossResourceMove disabled, slot returns old value
-                    if (config.crossResourceMove === false) {
-                        slotId = schedulerData._getEventSlotId(item);
-                        slotName = undefined;
-                        let slot = schedulerData.getSlotById(slotId);
-                        if (!!slot)
-                            slotName = slot.name;
-                    }
-
-                    action = 'Move';
-                }
-
-                if (!!movingEvent) {
-                    movingEvent(schedulerData, slotId, slotName, newStart, newEnd, action, type, item);
-                }
-            },
-
-            canDrop: (props, monitor) => {
-                const { schedulerData, resourceEvents } = props;
-                const item = monitor.getItem();
-                if (schedulerData._isResizing()) return false;
-                const { config } = schedulerData;
-                return config.movable && !resourceEvents.groupOnly && (item.movable == undefined || item.movable !== false);
-            }
+  getDropSpec = () => {
+    return {
+      drop: (props, monitor, component) => {
+        const { schedulerData, resourceEvents } = props;
+        const { cellUnit, localeDayjs } = schedulerData;
+        const type = monitor.getItemType();
+        const pos = getPos(component.eventContainer);
+        let cellWidth = schedulerData.getContentCellWidth();
+        let initialStartTime = null, initialEndTime = null;
+        if (type === DnDTypes.EVENT) {
+          const initialPoint = monitor.getInitialClientOffset();
+          let initialLeftIndex = Math.floor((initialPoint.x - pos.x) / cellWidth);
+          initialStartTime = resourceEvents.headerItems[initialLeftIndex].start;
+          initialEndTime = resourceEvents.headerItems[initialLeftIndex].end;
+          if (cellUnit !== CellUnit.Hour)
+            initialEndTime = localeDayjs(new Date(resourceEvents.headerItems[initialLeftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
         }
-    }
+        const point = monitor.getClientOffset();
+        let leftIndex = Math.floor((point.x - pos.x) / cellWidth);
+        let startTime = resourceEvents.headerItems[leftIndex].start;
+        let endTime = resourceEvents.headerItems[leftIndex].end;
+        if (cellUnit !== CellUnit.Hour)
+          endTime = localeDayjs(new Date(resourceEvents.headerItems[leftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
 
-    getDropCollect = (connect, monitor) => {
         return {
-            connectDropTarget: connect.dropTarget(),
-            isOver: monitor.isOver()
+          slotId: resourceEvents.slotId,
+          slotName: resourceEvents.slotName,
+          start: startTime,
+          end: endTime,
+          initialStart: initialStartTime,
+          initialEnd: initialEndTime
         };
-    }
+      },
 
-    getDropTarget = (dragAndDropEnabled) => {
-        return (dragAndDropEnabled ?
-            DropTarget([...this.sourceMap.keys()], this.getDropSpec(), this.getDropCollect)(this.DecoratedComponent) :
-            (this.DecoratedComponent)
-        );
-    }
+      hover: (props, monitor, component) => {
 
-    getDndSource = (dndType = DnDTypes.EVENT) => {
-        return this.sourceMap.get(dndType);
+        const { schedulerData, resourceEvents, movingEvent } = props;
+        const { cellUnit, config, viewType, localeDayjs } = schedulerData;
+        this.config = config;
+        const item = monitor.getItem();
+        const type = monitor.getItemType();
+        const pos = getPos(component.eventContainer);
+        let cellWidth = schedulerData.getContentCellWidth();
+        let initialStart = null, initialEnd = null;
+        if (type === DnDTypes.EVENT) {
+          const initialPoint = monitor.getInitialClientOffset();
+          let initialLeftIndex = Math.floor((initialPoint.x - pos.x) / cellWidth);
+          initialStart = resourceEvents.headerItems[initialLeftIndex].start;
+          initialEnd = resourceEvents.headerItems[initialLeftIndex].end;
+          if (cellUnit !== CellUnit.Hour)
+            initialEnd = localeDayjs(new Date(resourceEvents.headerItems[initialLeftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
+        }
+        const point = monitor.getClientOffset();
+        let leftIndex = Math.floor((point.x - pos.x) / cellWidth);
+        if (!resourceEvents.headerItems[leftIndex]) {
+          return;
+        }
+        let newStart = resourceEvents.headerItems[leftIndex].start;
+        let newEnd = resourceEvents.headerItems[leftIndex].end;
+        if (cellUnit !== CellUnit.Hour)
+          newEnd = localeDayjs(new Date(resourceEvents.headerItems[leftIndex].start)).hour(23).minute(59).second(59).format(DATETIME_FORMAT);
+        let slotId = resourceEvents.slotId, slotName = resourceEvents.slotName;
+        let action = 'New';
+        let isEvent = type === DnDTypes.EVENT;
+        if (isEvent) {
+          const event = item;
+          if (config.relativeMove) {
+            newStart = localeDayjs(event.start).add(localeDayjs(newStart).diff(localeDayjs(new Date(initialStart))), 'ms').format(DATETIME_FORMAT);
+          } else {
+            if (viewType !== ViewType.Day) {
+              let tmpDayjs = localeDayjs(newStart);
+              newStart = localeDayjs(event.start).year(tmpDayjs.year()).month(tmpDayjs.month()).date(tmpDayjs.date()).format(DATETIME_FORMAT);
+            }
+          }
+          newEnd = localeDayjs(newStart).add(localeDayjs(event.end).diff(localeDayjs(event.start)), 'ms').format(DATETIME_FORMAT);
+
+          //if crossResourceMove disabled, slot returns old value
+          if (config.crossResourceMove === false) {
+            slotId = schedulerData._getEventSlotId(item);
+            slotName = undefined;
+            let slot = schedulerData.getSlotById(slotId);
+            if (!!slot)
+              slotName = slot.name;
+          }
+
+          action = 'Move';
+        }
+
+        if (!!movingEvent) {
+          movingEvent(schedulerData, slotId, slotName, newStart, newEnd, action, type, item);
+        }
+      },
+
+      canDrop: (props, monitor) => {
+        const { schedulerData, resourceEvents } = props;
+        const item = monitor.getItem();
+        if (schedulerData._isResizing()) return false;
+        const { config } = schedulerData;
+        return config.movable && !resourceEvents.groupOnly && (item.movable == undefined || item.movable !== false);
+      }
     }
+  }
+
+  getDropCollect = (connect, monitor) => {
+    return {
+      connectDropTarget: connect.dropTarget(),
+      isOver: monitor.isOver()
+    };
+  }
+
+  getDropTarget = (dragAndDropEnabled) => {
+    return (dragAndDropEnabled ?
+      DropTarget([...this.sourceMap.keys()], this.getDropSpec(), this.getDropCollect)(this.DecoratedComponent) :
+      (this.DecoratedComponent)
+    );
+  }
+
+  getDndSource = (dndType = DnDTypes.EVENT) => {
+    return this.sourceMap.get(dndType);
+  }
 }
--- webpack/webpack-dev.config.js
+++ webpack/webpack-dev.config.js
@@ -1,20 +1,24 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const root = path.resolve(__dirname, '..');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    'basic': [
-      'webpack-dev-server/client?http://localhost:8080/',
-      'webpack/hot/only-dev-server',
-      './example/bundle.js'
-    ]
-  },
+  // entry: {
+  //   'basic': [
+  //     'webpack-dev-server/client?http://localhost:8080/',
+  //     'webpack/hot/only-dev-server',
+  //     './example/bundle.js'
+  //   ]
+  // },
+  entry: ['./example/bundle.js'],
   output: {
     path: __dirname,
-    filename: "bundle.js",
+    //path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[fullhash].js',
     publicPath: 'http://localhost:8080/',
     chunkFilename: '[id].chunk.js',
     sourceMapFilename: '[name].map'
@@ -25,34 +29,55 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.jsx$|\.es6$|\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+      {
+        test: /\.jsx$|\.es6$|\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        },
+      },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              sourceMap: false
-            }
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: false
-            }
-          },
-        ],
+        use: ["style-loader", "css-loader"],
       },
-      { test: /\.(jpe?g|png|gif)$/i, loader: 'url?limit=10000!img?progressive=true' },
-      { test: /\.json/, loader: 'json-loader', exclude: /node_modules/ }
+      { test: /\.(jpe?g|png|gif)$/i, type: 'asset/resource' },
+      { test: /\.json/, type: 'json', exclude: /node_modules/ }
     ]
   },
   plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.NormalModuleReplacementPlugin(
-      /node_modules\/antd\/lib\/style\/index\.less/,
-      path.resolve(root, 'src/less/antd-globals-hiding-hack.less')
-    ),
+    // new webpack.NoEmitOnErrorsPlugin(),
+    // new webpack.NormalModuleReplacementPlugin(
+    //   /node_modules\/antd\/lib\/style\/index\.less/,
+    //   path.resolve(root, 'src/less/antd-globals-hiding-hack.less')
+    // ),
+    new HtmlWebpackPlugin({
+      template: 'example/index.html',
+    }),
+    // new WebpackPluginServe({
+    //   host: 'localhost',
+    //   port: 8080,
+    //   historyFallback: true,
+    //   open: true,
+    //   liveReload: false,
+    //   hmr: true,
+    //   static: './dist',
+    // }),
   ],
-  devtool: "none"
+  devServer: {
+    allowedHosts: 'auto',
+    client: {
+      overlay: true,
+    },
+    hot: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(root, 'src/css'),
+    },
+  },
+  devtool: "eval"
 };
